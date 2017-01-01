48b22c2fd23576cfb861a7739cefb976310ae88a
randy pen <pencoa42@gmail.com>
2017-01-01 15:40:23 +0800

    including third part library in systemconfig

--- systemjs.config.js
+++ systemjs.config.js
@@ -25,7 +25,8 @@
 
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'ng2-uploader':              'npm:ng2-uploader'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -34,6 +35,10 @@
         defaultExtension: 'js'
       },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      'ng2-uploader': {
+        main: 'ng2-uploader.js',
         defaultExtension: 'js'
       }
     }
