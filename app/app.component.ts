273d50d46ba53528e5116adcd6e6362996a010b9
randy pen <pencoa42@gmail.com>
2017-01-01 15:59:08 +0800

    update to NG rc6 structure

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/demo.html'
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+	uploadFile: any;
+	  options: Object = {
+	    url: 'http://localhost:10050/upload'
+	  };
+
+	  handleUpload(data): void {
+	    if (data && data.response) {
+	      data = JSON.parse(data.response);
+	      this.uploadFile = data;
+	    }
+	  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {UPLOAD_DIRECTIVES} from 'ng2-uploader';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    UPLOAD_DIRECTIVES
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
